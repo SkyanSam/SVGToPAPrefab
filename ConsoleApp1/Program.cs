66d88fa4a93e3e8b4f95c04d0d4b43bed11f8503
SkyanSam <samuelwiggins15@gmail.com>
2020-04-13 21:52:49 -0400

    Rotated Shapes! #1
    - Implemented squares, hexagons, triangles.
    
    Squares are succesfull! Hexagons not tested! And triangles may work or may not work
    
    Mental Note: Need to set ellipse width and height * 2.
    
    Mental Note: need to decide whether we should use clockwise / anticlockwise rotation and if we should do 0 -> 360 or -360 -> 360.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Xml;
 using System.Linq;
+using System.Numerics;
 
 namespace ConsoleApp1
 {
@@ -14,7 +15,7 @@ namespace ConsoleApp1
         public static string prefabName = "Prefab Generated From SVG";
         public static PrefabType prefabType = PrefabType.Misc1;
         public static float secondsToLast = 15f;
-        public static string svgPath = @"D:/Documents/CSharpProjects/AbandonedBoss.svg";
+        public static string svgPath = @"D:/Documents/CSharpProjects/RotatedHell.svg";
         public static string prefabPath = @"C:\Program Files (x86)\Steam\steamapps\common\Project Arrhythmia\beatmaps\prefabs";
     }
     class Colors {
@@ -328,7 +329,7 @@ namespace ConsoleApp1
             {
                 shape = null;
                 // Splitting the values
-                string[] rawArray = nextVal.Split('M', 'L', 'Z', ' ');
+                string[] rawArray = nextVal.Split('M', 'L', 'Z', ' ', 'z', 'l', 'm');
                 ArrayList arraylist = new ArrayList();
 
                 // We dont want to add empty stuff
@@ -344,28 +345,57 @@ namespace ConsoleApp1
                 for (int i = 0; i < arraylist.Count; i++)
                     floatArray[i] = float.Parse((string)arraylist[i]);
 
-                // Analyze the number of points. We multiply by two because its split up by float, not by vector / float[2] then add two because of [M x y]
-                if (floatArray.Length == (3 * 2)+2) shape = Shapes.Triangle;
-                else if (floatArray.Length == (6 * 2)+2) shape = Shapes.Hexagon;
-
                 Console.WriteLine(floatArray.Length);
-                if (floatArray.Length == 3 * 2) Console.WriteLine("ITS A FUCKING TRIANGLE");
                 // If nothing else we can assume the shape is null.
 
                 // Create float array for x and y.
-                float[] xArray = new float[floatArray.Length / 2];
-                float[] yArray = new float[floatArray.Length / 2];
+                ArrayList xArrayList = new ArrayList();
+                ArrayList yArrayList = new ArrayList();
+
+                // Length of x&y array list = floatArray.Length / 2
                 for (int i = 0; i < floatArray.Length; i += 2)
                 {
-                    xArray[i / 2] = floatArray[i];
+                    xArrayList.Add(floatArray[i]);
                 }
-                for (int[] i = new int[] { 1, 0 }; i[0] < floatArray.Length; i[0] += 2)
+                for (int i = 1; i < floatArray.Length; i += 2)
                 {
-                    // i[0] is index of floatArray
-                    // i[1] is index of yArray
-                    yArray[i[1]] = floatArray[i[0]];
-                    i[1] += 1;
+                    yArrayList.Add(floatArray[i]);
                 }
+
+                // Remove points if there is the same 2 points
+                Vector2 startingPoint = new Vector2((float)xArrayList[0], (float)yArrayList[0]);
+                for (int i = 1; i < xArrayList.Count; i++)
+                {
+                    if (startingPoint.X == (float)xArrayList[i] && startingPoint.Y == (float)yArrayList[i])
+                    {
+                        xArrayList.RemoveAt(i);
+                        yArrayList.RemoveAt(i);
+                    }
+                }
+
+                // DEBUG
+                Console.WriteLine("DEBUG LIST ///");
+                for(int i = 0; i < xArrayList.Count; i++)
+                {
+                    Console.WriteLine((float)xArrayList[i] + "  ,  " + (float)yArrayList[i]);
+                }
+                Console.WriteLine("END DEBUG ///");
+                //
+
+                float[] xArray = (float[])xArrayList.ToArray(typeof(float));
+                float[] yArray = (float[])yArrayList.ToArray(typeof(float));
+
+
+                Vector2[] points = new Vector2[xArray.Length];
+                for (int i = 0; i < xArray.Length; i++)
+                {
+                    points[i] = new Vector2(xArray[i], yArray[i]);
+                }
+
+                // Analyze the number of points. We multiply by two because its split up by float, not by vector / float[2] then add two because of [M x y]
+                if (points.Length == 3) shape = Shapes.Triangle;
+                else if (points.Length == 4) shape = Shapes.Square;
+                else if (points.Length == 6) shape = Shapes.Hexagon;
 
                 // Get min and max
                 float[] min = new float[] { xArray.Min(), yArray.Min() };
@@ -377,17 +407,32 @@ namespace ConsoleApp1
                 Console.WriteLine("minmax");
                 // Get center and size
                 float[] center = CustomMath.GetCenter(min, max);
-                float[] size = CustomMath.GetSize(min, max);
+
+                // Everything with final means its been rotated.
+                Vector2[] finalPoints;
+                float finalRotation;
+                GetRotatedShape(1f, CustomConversions.Float2ToVect(center), points, out finalPoints, out finalRotation);
+
+                float[] finalXPoints = CustomConversions.VectIndexToFloatList(0, finalPoints);
+                float[] finalYPoints = CustomConversions.VectIndexToFloatList(1, finalPoints);
+
+                float[] finalMin = new float[] { finalXPoints.Min(), finalYPoints.Min() };
+                float[] finalMax = new float[] { finalXPoints.Max(), finalYPoints.Max() };
+
+                float[] finalSize = CustomMath.GetSize(finalMin, finalMax);
+
+
                 Console.WriteLine(center[0]);
                 Console.WriteLine(center[1]);
-                Console.WriteLine(size[0]);
-                Console.WriteLine(size[1]);
+                Console.WriteLine(finalSize[0]);
+                Console.WriteLine(finalSize[1]);
 
                 // Apply center and size
                 obj.positionX = center[0];
                 obj.positionY = center[1];
-                obj.sizeX = size[0];
-                obj.sizeY = size[1];
+                obj.sizeX = finalSize[0];
+                obj.sizeY = finalSize[1];
+                obj.rotAngle = finalRotation;
             }
             public static void X(ref GameObjectData obj, float nextValf)
             {
@@ -433,6 +478,94 @@ namespace ConsoleApp1
                 float[] center = GetCenter(min, max);
                 return new float[] { (max[0] - center[0])*2, (max[1] - center[1])*2};
             }
+            public struct RotatePtsAroundOrigin {
+                public static void AntiClockwise(float rotRad, Vector2[] points, out Vector2[] rotatedPoints)
+                {
+                    // Note that this is radians.
+                    rotatedPoints = new Vector2[points.Length];
+                    for (int i = 0; i < points.Length; i++)
+                    {
+                        rotatedPoints[i].X = (points[i].X * MathF.Cos(rotRad)) - (points[i].Y * MathF.Sin(rotRad));
+                        rotatedPoints[i].Y = (points[i].X * MathF.Sin(rotRad)) + (points[i].Y * MathF.Cos(rotRad));
+                    }
+                } 
+            }
+            
+        }
+        struct CustomConversions
+        {
+            public static float[] VectToFloat2(Vector2 vector)
+            {
+                return new float[] { vector.X, vector.Y };
+            }
+            public static Vector2 Float2ToVect(float[] float2)
+            {
+                return new Vector2(float2[0], float2[1]);
+            }
+            public static float[] VectIndexToFloatList(int index, Vector2[] vectors)
+            {
+                float[] rFloatArray = new float[vectors.Length];
+                for (int i = 0; i < vectors.Length; i++) 
+                    if (index == 1) rFloatArray[i] = vectors[i].Y; 
+                    else rFloatArray[i] = vectors[i].X;
+
+                return rFloatArray;
+            }
+        }
+        public static void GetRotatedShape(float rotDegDiff, Vector2 center, Vector2[] points, out Vector2[] RotatedPointsOut, out float finalRotation)
+        {
+            // Default Values.
+            finalRotation = 0;
+            RotatedPointsOut = points;
+            //
+
+            var rotRadDiff = rotDegDiff * (MathF.PI / 180); // Converting deg -> rad
+            for (int v = 0; v < points.Length; v++) points[v] -= center; // Subtracting the diff so we can rotate around the center.
+
+            for (float r = -(MathF.PI * 2); r < MathF.PI * 2; r += rotRadDiff) // questioning if we should do -360 -> 360 or -360 -> 0
+            {
+                Vector2[] rotatedPoints;
+                CustomMath.RotatePtsAroundOrigin.AntiClockwise(r, points, out rotatedPoints);
+
+                for (int i = 1; i < rotatedPoints.Length; i++)
+                {
+                    // We want the base line to be on the x axis.
+                    var absDist = MathF.Abs(rotatedPoints[i].Y - rotatedPoints[i - 1].Y);
+                    // Comparing it to the deadzone.
+                    if (absDist < 0.5f && rotatedPoints[i].Y > 0) // We want it to be the lowest line below everything. (closest to width)
+                    { 
+                        // If this is a triangle
+                        if (points.Length == 3)
+                        {
+                            Vector2 otherpoint = points[0];
+                            if (i == 1) otherpoint = points[2];
+                            if (i == 2) otherpoint = points[0];
+                            if (MathF.Abs(otherpoint.X) > 1)
+                            {
+                                // We dont want to continue if the top point of the triangle isnt close to the center.
+                                // I'll consider a function to analyze the other part of the triangle to see if its a right triangle later.
+                                continue;
+                            }
+                        }
+
+                        // If we got this far we can assume this is the correct rotation and points
+                        RotatedPointsOut = rotatedPoints;
+                        finalRotation = -r / (MathF.PI / 180);
+                    }
+
+                }
+            }
+
+            // Questioning if recalculating the center makes results more accurate, if not consider removing.
+            float[] xArray = CustomConversions.VectIndexToFloatList(0, RotatedPointsOut);
+            float[] yArray = CustomConversions.VectIndexToFloatList(1, RotatedPointsOut);
+            float[] finalCenter = CustomMath.GetCenter(
+                new float[] { xArray.Min(), yArray.Min() },
+                new float[] { xArray.Max(), yArray.Max() }
+            );
+            //
+
+            for (int v = 0; v < points.Length; v++) points[v] += CustomConversions.Float2ToVect(finalCenter); // Adding back the diff.
         }
     }
 }
